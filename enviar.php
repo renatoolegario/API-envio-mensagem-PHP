8ef6f9916d9fde02b165f7027be618768623aa7d
Renato Olegário <73908159+renatoolegario@users.noreply.github.com>
2022-09-13 13:36:47 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -18,7 +18,7 @@ $nome_titulo_email  = $_POST['nome_titulo_email'];
 $titulo_email       = $_POST['titulo_email']; 
 $corpo_email        = $_POST['corpo_email'];
 $identificador_msg  = $_POST['codigo_identificador'];
-$url_webhook        = $_POST['url_webhook']; // será aqui que será enviado o disparo da desiscrição via cliente // LINK_API_DESISCRICAO
+$url_webhook        = $_POST['url_webhook']; // será aqui que será enviado o disparo da desiscrição via cliente // O WEBHOOK DEVERÁ RECEBER VIA GET A VARIAVEL IDENTICACAO
 
 $body               = file_get_contents($corpo_email);
 
@@ -59,6 +59,41 @@ $FRASE_DA_EMPRESA 				= $_POST['FRASE_DA_EMPRESA'];
 
 // fim das variáveis 
 // Aqui inicia-se a substituição das variavies do anexo pelos POST recebidos.
+
+$body = str_replace(' $$ITEM_01_LISTAGEM$$',$ITEM_01_LISTAGEM,$body);
+$body = str_replace(' $$ITEM_02_LISTAGEM$$',$ITEM_02_LISTAGEM,$body);
+$body = str_replace(' $$ITEM_03_LISTAGEM$$',$ITEM_03_LISTAGEM,$body);
+$body = str_replace(' $$ITEM_04_LISTAGEM$$',$ITEM_04_LISTAGEM,$body);
+$body = str_replace(' $$ITEM_05_LISTAGEM$$',$ITEM_05_LISTAGEM,$body);
+$body = str_replace(' $$ITEM_06_LISTAGEM$$',$ITEM_06_LISTAGEM,$body);
+$body = str_replace(' $$ITEM_07_LISTAGEM$$',$ITEM_07_LISTAGEM,$body);
+$body = str_replace(' $$ITEM_08_LISTAGEM$$',$ITEM_08_LISTAGEM,$body);
+$body = str_replace(' $$ITEM_09_LISTAGEM$$',$ITEM_09_LISTAGEM,$body);
+$body = str_replace(' $$DESCRICAO_ABAIXO_LISTA$$',$DESCRICAO_ABAIXO_LISTA,$body);
+$body = str_replace(' $$TITUTLO_DESCRICAO_1$$',$TITULO_DESCRICAO_1,$body);
+$body = str_replace(' $$DESCRICAO_1$$',$DESCRICAO_1,$body);
+$body = str_replace(' $$TITUTLO_DESCRICAO_2$$',$TITULO_DESCRICAO_2,$body);
+$body = str_replace(' $$DESCRICAO_2$$',$DESCRICAO_2,$body);
+$body = str_replace(' $$TITUTLO_DESCRICAO_3$$',$TITULO_DESCRICAO_3,$body);
+$body = str_replace(' $$DESCRICAO_3$$',$DESCRICAO_3,$body);
+$body = str_replace(' $$TITUTLO_DESCRICAO_4$$',$TITULO_DESCRICAO_4,$body);
+$body = str_replace(' $$DESCRICAO_4$$',$DESCRICAO_4,$body);
+$body = str_replace(' $$TITUTLO_DESCRICAO_5$$',$TITULO_DESCRICAO_5,$body);
+$body = str_replace(' $$DESCRICAO_5$$',$DESCRICAO_5,$body);
+$body = str_replace(' $$DESCRICAO_FINAL$$',$DESCRICAO_FINAL,$body);
+$body = str_replace(' $$LINK_BT_1$$',$LINK_BT_1,$body);
+$body = str_replace('$$IMG_BT_1$$',$IMG_BT_1,$body);
+$body = str_replace('$$DESCRICAO_BT_1$$',$DESCRICAO_BT_1,$body);
+$body = str_replace(' $$LINK_BT_2$$',$LINK_BT_2,$body);
+$body = str_replace(' $$IMG_BT_2$$',$IMG_BT_2,$body);
+$body = str_replace(' $$DESCRICAO_BT_2$$',$DESCRICAO_BT_2,$body);
+$body = str_replace(' $$LINK_BT_3$$',$LINK_BT_3,$body);
+$body = str_replace(' $$DESCRICAO_BT_3$$',$DESCRICAO_BT_3,$body);
+$body = str_replace(' $$LINK_BANNER_BOTTOM$$',$LINK_BANNER_BOTTOM,$body);
+$body = str_replace(' $$IMG_BANNER_BOTTOM$$',$IMG_BANNER_BOTTOM,$body);
+$body = str_replace(' $$NOME_DA_EMPRESA$$',$NOME_DA_EMPRESA,$body);
+$body = str_replace(' $$FRASE_DA_EMPRESA$$',$FRASE_DA_EMPRESA,$body);
+$body = str_replace(' $$LINK-API-DESISCRICAO$$',$url_webhook,$body);
 
 
 //Fim das substituições
