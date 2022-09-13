9b2e1e29ba75c8ab5e81998e59635e7a902c6205
Renato Olegário <73908159+renatoolegario@users.noreply.github.com>
2022-09-13 10:53:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,3 +8,47 @@ Você deve enviar os seguintes paramentros no <strong>POST</strong> para o ender
 <strong>Nome do Responsável do E-mail: </strong>Renato Olegário</br>
 <strong>Título do E-mail: </strong> Seja muito bem vindo a MultiplasFR.</br>
 <strong>Endereço do HTML do corpo do e-mail: </strong>https://email.multiplasfr.com.br/modelos/modelo_01.html</br>
+<hr>
+<strong> Envie estas variáveis no POST para utilizar o modelo_01.html </strong></br>
+$$IMAGEM_TOPO$$</br>
+$$IMAGEM_BEIRADA_ESQUERDA$$</br>
+$$TITULO_SUPERIOR$$</br>
+$$IMAGEM_BEIRADA_DIREITA$$</br>
+$$SUB_TITULO_SUPERIOR$$</br>
+$$DESCRICAO_SUB_TITULO_SUPERIOR$$ </br>
+$$DESCRICAO_DA_LISTAGEM_SUPERIOR$$  </br>
+$$ITEM_01_LISTAGEM$$</br>
+$$ITEM_02_LISTAGEM$$</br>
+$$ITEM_03_LISTAGEM$$</br>
+$$ITEM_04_LISTAGEM$$</br>
+$$ITEM_05_LISTAGEM$$</br>
+$$ITEM_06_LISTAGEM$$</br>
+$$ITEM_07_LISTAGEM$$</br>
+$$ITEM_08_LISTAGEM$$</br>
+$$ITEM_09_LISTAGEM$$</br>
+$$DESCRICAO_ABAIXO_LISTA$$ </br>
+$$TITUTLO_DESCRICAO_1$$</br>
+$$DESCRICAO_1$$</br>
+$$TITUTLO_DESCRICAO_2$$ </br>
+$$DESCRICAO_2$$</br>
+$$TITUTLO_DESCRICAO_3$$</br>
+$$DESCRICAO_3$$</br>
+$$TITUTLO_DESCRICAO_4$$</br>
+$$DESCRICAO_4$$</br>
+$$TITUTLO_DESCRICAO_5$$</br>
+$$DESCRICAO_5$$</br>
+$$DESCRICAO_FINAL$$</br>
+$$LINK_BT_1$$</br>
+$$IMG_BT_1$$</br>
+$$DESCRICAO_BT_1$$</br>
+$$LINK_BT_2$$</br>
+$$IMG_BT_2$$</br>
+$$DESCRICAO_BT_2$$</br>
+$$LINK_BT_3$$</br>
+$$DESCRICAO_BT_3$$</br>
+$$LINK_BANNER_BOTTOM$$</br>
+$$IMG_BANNER_BOTTOM$$</br>
+$$NOME_DA_EMPRESA$$</br>
+$$FRASE_DA_EMPRESA$$</br>
+$$LINK-API-DESISCRICAO$$</br>
+
